24a914950c5c3d63e139462c0818d81855a1a589
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-11-06 08:20:46 -0300

    Fix: remove search(temp)

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,7 +4,7 @@ import '@docsearch/css';
 
 const Search: React.FC = () => {
 //   return <DocSearch appId="8XXXVNJ7UW" apiKey="bf7661918f784d59105bc70fcc22bfb0" indexName="overextended" />;
-return '';
+return null;
 };
 
 export default Search;
