6fb547e7538b204ecece62f00302f562f1ce87bc
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-11-06 16:55:18 -0300

    Feat: retorna search

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,8 +3,7 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search: React.FC = () => {
-//   return <DocSearch appId="8XXXVNJ7UW" apiKey="bf7661918f784d59105bc70fcc22bfb0" indexName="overextended" />;
-return null;
+  return <DocSearch appId="876KCB22DI" apiKey="99a2c8ae162662ba8701a409923928ae" indexName="mriqbox-com" />;
 };
 
 export default Search;
