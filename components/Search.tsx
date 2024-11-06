449b7e1646fbf2d25213093c7211660828463d2b
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-11-06 08:17:11 -0300

    Fix: remove search(temp)

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,7 +3,8 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search: React.FC = () => {
-  return <DocSearch appId="8XXXVNJ7UW" apiKey="bf7661918f784d59105bc70fcc22bfb0" indexName="overextended" />;
+//   return <DocSearch appId="8XXXVNJ7UW" apiKey="bf7661918f784d59105bc70fcc22bfb0" indexName="overextended" />;
+return '';
 };
 
 export default Search;
