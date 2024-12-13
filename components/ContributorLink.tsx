a79af5efdc588b612be235c4efc0af4fd7fa2c92
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-12-13 20:23:04 -0300

    Fix: aumentado tamanho do botão para melhorar UX

--- components/ContributorLink.tsx
+++ components/ContributorLink.tsx
@@ -2,7 +2,7 @@ import { IconBrandGithubFilled } from '@tabler/icons-react';
 
 const ContributorLink: React.FC<{ image: string; username: string }> = ({ image, username }) => {
   return (
-    <div className="flex w-32 flex-col items-center justify-center gap-2">
+    <div className="flex w-40 flex-col items-center justify-center gap-2">
       <img src={image} alt={`${username}-image`} className="rounded-full bg-neutral-100 shadow-md dark:bg-neutral-900" />
       <a href={`https://github.com/${username}`} target="_blank" className="w-full">
         <button className="flex w-full items-center justify-center gap-2 rounded-md bg-green-500/20 p-2 text-green-500 hover:bg-green-500/30">
