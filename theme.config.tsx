147aa773bac5d9cc32f0c1d20d378c3601c18613
Gabriel Freitas <ggfto@outlook.com>
2026-05-08 17:20:47 -0300

    chore: migra de Nextra v2 para v3
    - Atualiza nextra e nextra-theme-docs para ^3.0.0
    - Atualiza TypeScript para ^6.0.3 (suporte a const type params)
    - Substitui next.config.js por next.config.mjs com ESM
    - Substitui pages/_app.mdx por pages/_app.tsx
    - Adapta theme.config.tsx para API v3 (color, footer.content, useHead)
    - Remove patch de nextra@2.13.4 obsoleto
    - Adiciona typescript.ignoreBuildErrors para compatibilidade temporária

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,11 +6,23 @@ import Search from '@components/Search';
 function useHead() {
   const { asPath } = useRouter();
   const { frontMatter, title } = useConfig();
+
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'mri-Qbox';
+  const rawTitle = arr[arr.length - 1];
+  const pageTitle = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : title;
+  const fullTitle = `${pageTitle} - ${
+    rawTitle === category
+      ? 'Documentação'
+      : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+  }`;
+
   const url = `https://docs.mriqbox.com.br${asPath}`;
   const description = frontMatter.description || "Documentação para os recursos desenvolvidos pela mri-Qbox.";
 
   return (
     <>
+      <title>{fullTitle}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/x-icon" href="/static/mri.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -21,25 +33,11 @@ function useHead() {
       />
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content={description} />
-      <meta name="og:title" content={title} />
+      <meta name="og:title" content={fullTitle} />
       <meta name="og:description" content={description} />
       <meta name="og:url" content={url} />
     </>
   );
-}
-
-function useNextSeoProps() {
-  const { asPath } = useRouter();
-  const arr = asPath.replace(/[-_]/g, ' ').split('/');
-  const category = (arr[1][0] !== '#' && arr[1]) || 'mri-Qbox';
-  const rawTitle = arr[arr.length - 1];
-  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
-
-  return {
-    titleTemplate: `${title} - ${
-      rawTitle === category ? 'Documentação' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
-    }`,
-  };
 }
 
 const config: DocsThemeConfig = {
@@ -64,22 +62,22 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/mri-Qbox-Brasil/docs-mriqbox/blob/main',
   footer: {
-    text: 'Copyright © 2026 MRI QBOX BRASIL. Todos os direitos reservados.',
+    content: 'Copyright © 2026 MRI QBOX BRASIL. Todos os direitos reservados.',
   },
   search: {
     component: <Search />,
   },
   head: useHead,
-  primaryHue: { dark: 160, light: 160 },
-  primarySaturation: { dark: 100, light: 100 },
+  color: {
+    hue: { dark: 160, light: 160 },
+    saturation: { dark: 100, light: 100 },
+  },
   sidebar: {
     defaultMenuCollapseLevel: 1,
   },
   toc: {
     backToTop: true,
   },
-
-  useNextSeoProps: useNextSeoProps,
 };
 
 export default config;
